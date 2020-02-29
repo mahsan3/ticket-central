e5c40f6077b5b6861df58ce8dbceda28b000e515
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-29 14:44:44 -0500

    Added body-parser middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const appRoutes = require('./api/routes/routes');
 const models = require('./api/models');
+
+app.use(bodyParser.json());
 
 app.use('/', appRoutes);
 
