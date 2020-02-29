0fb6860ecbf810ebf2a9214c1732f70b51411397
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-29 15:20:13 -0500

    Updated controller for querying all tickets, with associations

--- api/controllers/tickets/get/getTickets.js
+++ api/controllers/tickets/get/getTickets.js
@@ -1,5 +1,49 @@
+const models = require('../../../models');
+
 module.exports = async (req, res, next) => {
-    res.json({
-        message: 'great success.'
-    });
+
+    try {
+
+        const tickets = await models.ticket.findAll({
+            include: [{
+                model: models.user,
+                as: 'ticket_creator',
+                attributes: ['name']
+            }, {
+                model: models.user,
+                as: 'ticket_assigned_to',
+                attributes: ['name']
+            }, {
+                model: models.status,
+                attributes: ['name']
+            }, {
+                model: models.tag,
+                attributes: ['name'],
+                through: {
+                    attributes: [] // don't need any attributes from join table
+                }
+            }]
+        });
+
+        console.log(tickets);
+
+        res.json({
+            success: true,
+            error: false,
+            message: '',
+            data: {
+                tickets
+            }
+        });
+
+    }catch(err) {
+
+        res.json({
+            success: false,
+            error: true,
+            message: err.message
+        });
+
+    }
+
 };
