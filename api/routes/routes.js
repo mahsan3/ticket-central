5fe5ca1bba37f494a05d8168f909c1b43f419032
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-29 12:31:25 -0500

    Wiring up new ticket routes to app

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,12 +1,11 @@
 // Brings together all the other routes here
 const routes = require('express').Router();
+const ticketRoutes = require('./ticket.routes');
 
 routes.get('/heart-beat', (req, res) => {
     res.status(200).json({ message: 'API is up.' });
 });
 
-routes.use('/user', () => {});
-routes.use('/ticket', () => {});
-
+routes.use('/tickets', ticketRoutes);
 
 module.exports = routes;
