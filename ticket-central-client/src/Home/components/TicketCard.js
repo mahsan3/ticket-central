545f2217da85432ccf6d817b8d0de6bf3c25f404
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 13:46:06 -0500

    Added simple card component to present ticket data

---
+++ ticket-central-client/src/Home/components/TicketCard.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Moment from 'react-moment';
+
+function TicketCard({ticket}) {
+    return (
+        <div className="card m-2">
+            <div className="card-body">
+                <div className="d-flex justify-content-between">
+                    <h5 className="card-title">{ticket.title}</h5>
+                    <button type="button" className="btn btn-outline-primary">
+                        <i className="far fa-edit"></i>
+                    </button>
+                </div>
+
+                <h6 className="card-subtitle mb-2 text-muted">
+                    <Moment format="MMMM DD, YYYY">
+                        {ticket.createdAt}
+                    </Moment>
+                </h6>
+                <p className="card-text">
+                    {ticket.description}
+                </p>
+                <footer className="">
+                    {ticket.tags.map((t, i) => <span key={i} className="badge badge-pill badge-info">{t.name}</span>
+                    )}
+                </footer>
+            </div>
+        </div>
+    );
+}
+
+export default TicketCard;
