a713d5e636b1f9b1c68789f5f1ad75c3cf0dc988
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 16:31:57 -0500

    Added an epic for loading ticket drop down data

--- ticket-central-client/src/store/epics/ticket.epic.js
+++ ticket-central-client/src/store/epics/ticket.epic.js
@@ -1,10 +1,17 @@
-import {LOAD_ALL_TICKETS, loadAllTicketsFail, loadAllTicketsSuccess} from "../actions/ticket.actions";
+import {
+    EDIT_TICKET,
+    LOAD_ALL_TICKETS, LOAD_TICKET_OPTION_DATA,
+    LOAD_TICKET_OPTION_DATA_SUCCESS,
+    loadAllTicketsFail,
+    loadAllTicketsSuccess, loadTicketOptionDataSuccess
+} from "../actions/ticket.actions";
 import {ofType} from "redux-observable";
 import {of} from 'rxjs';
 import {map, mergeMap, catchError} from "rxjs/operators";
 import { ajax } from 'rxjs/ajax';
+import history from "../../utils/history";
 
-export const loadTickets = action$ => action$.pipe(
+export const loadTicketsEpic = action$ => action$.pipe(
     ofType(LOAD_ALL_TICKETS),
     mergeMap(action => ajax({
             url: '/api/ticket/all',
@@ -21,4 +28,31 @@ export const loadTickets = action$ => action$.pipe(
             })
         )
     )
+);
+
+export const loadTicketOptionsEpic = action$ => action$.pipe(
+    ofType(LOAD_TICKET_OPTION_DATA),
+    mergeMap(action => ajax({
+            url: '/api/ticket/optional-data',
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${action.payload}`
+            }
+        }).pipe(
+        map(response => {
+            return loadTicketOptionDataSuccess(response.response.data);
+        }),
+        catchError(err => {
+            return of("uh oh - spaghettios!");
+        })
+        )
+    )
+);
+
+export const editTicketEpic = action$ => action$.pipe(
+    ofType(EDIT_TICKET),
+    mergeMap(action => {
+        history.push(`/edit/${action.payload}`); // TODO: doesn't work, look into this
+        return of();
+    })
 );
