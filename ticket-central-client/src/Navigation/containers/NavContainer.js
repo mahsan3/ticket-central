a17c76d557678849b1710af353436105d80404fe
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-29 20:56:58 -0500

    Conditionally displaying login / logout links

--- ticket-central-client/src/Navigation/containers/NavContainer.js
+++ ticket-central-client/src/Navigation/containers/NavContainer.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import {useAuth0} from "../../Auth/Auth";
 
 function NavContainer(props) {
+
+        const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <a className="navbar-brand" href="#">Ticket Central</a>
@@ -15,9 +19,15 @@ function NavContainer(props) {
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
                 <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Login</a>
-                    </li>
+
+                    {!isAuthenticated && (
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#" onClick={() => loginWithRedirect({})}>Login</a>
+                        </li>
+                    )}
+                    {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+
+
                     <li className="nav-item">
                         <a className="nav-link" href="#"></a>
                     </li>
