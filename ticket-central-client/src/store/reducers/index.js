7a73e4c076bd5467f1efbc3521355faffb4e86a1
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 16:32:16 -0500

    Added missing import

--- ticket-central-client/src/store/reducers/index.js
+++ ticket-central-client/src/store/reducers/index.js
@@ -1,6 +1,6 @@
 import {combineReducers} from "redux";
 import { combineEpics } from 'redux-observable';
-import {loadTicketsEpic, editTicketEpic} from '../epics/ticket.epic';
+import {loadTicketsEpic, editTicketEpic, loadTicketOptionsEpic} from '../epics/ticket.epic';
 import ticketReducer from "./ticket.reducer";
 
 export const rootEpic = combineEpics(
