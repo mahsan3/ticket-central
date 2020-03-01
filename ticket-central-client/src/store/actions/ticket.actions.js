c8cdef7273773cf349a0c06bb7f5612ca05da71e
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 16:22:32 -0500

    Added actions for setting current selection and loading ticket options data

--- ticket-central-client/src/store/actions/ticket.actions.js
+++ ticket-central-client/src/store/actions/ticket.actions.js
@@ -1,7 +1,16 @@
 export const LOAD_ALL_TICKETS = 'LOAD_ALL_TICKETS';
 export const LOAD_ALL_TICKETS_SUCCESS = 'LOAD_ALL_TICKETS_SUCCESS';
 export const LOAD_ALL_TICKETS_FAIL = 'LOAD_ALL_TICKETS_FAIL';
+export const EDIT_TICKET = 'EDIT_TICKET';
+export const LOAD_TICKET_OPTION_DATA = 'LOAD_TICKET_OPTION_DATA';
+export const LOAD_TICKET_OPTION_DATA_SUCCESS = 'LOAD_TICKET_OPTION_DATA_SUCCESS';
+export const LOAD_TICKET_OPTION_DATA_FAIL = 'LOAD_TICKET_OPTION_DATA_FAIL';
 
+/**
+ * =====================================================
+ *                      LOAD TICKETS
+ * =====================================================
+ */
 export const loadAllTickets = jwt => ({
     type: LOAD_ALL_TICKETS,
     payload: jwt
@@ -15,3 +24,34 @@ export const loadAllTicketsSuccess = tickets => ({
 export const loadAllTicketsFail = () => ({
     type: LOAD_ALL_TICKETS_FAIL
 });
+
+
+/**
+ * =====================================================
+ *                      EDIT TICKET
+ * =====================================================
+ */
+export const editTicket = id => ({
+    type: EDIT_TICKET,
+    payload: id
+});
+
+/**
+ * =====================================================
+ *                 GET TICKET OPTION DATA
+ * =====================================================
+ */
+export const loadTicketOptionData = () => ({
+    type: LOAD_TICKET_OPTION_DATA
+});
+
+export const loadTicketOptionDataSuccess = (tags, status, users) => ({
+    type: LOAD_TICKET_OPTION_DATA_SUCCESS,
+    payload: {
+        tags,
+        status,
+        users
+    }
+});
+
+// TODO: skipping fail action for now...
