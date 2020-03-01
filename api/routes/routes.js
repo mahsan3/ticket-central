cc1f83e33c4a6070b6b5d0e03ab5f213e84fe811
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 13:25:39 -0500

    Locked down ticket routes to only logged in users

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,11 +1,12 @@
 // Brings together all the other routes here
 const routes = require('express').Router();
 const ticketRoutes = require('./ticket.routes');
+const checkJwt = require('../utils/check-jwt');
 
 routes.get('/heartbeat', (req, res) => {
     res.status(200).json({ message: 'API is up.' });
 });
 
-routes.use('/ticket', ticketRoutes);
+routes.use('/ticket', checkJwt, ticketRoutes);
 
 module.exports = routes;
