16f3983ec7e7b1c9e2d7b539a3ffb09ac939bae9
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 14:56:14 -0500

    Registered app epics and reducers

--- ticket-central-client/src/store/reducers/index.js
+++ ticket-central-client/src/store/reducers/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import { combineEpics } from 'redux-observable';
+import {loadTickets} from '../epics/ticket.epic';
+import ticketReducer from "./ticket.reducer";
 
 export const rootEpic = combineEpics(
-
+    loadTickets
 );
 
 export const rootReducer = combineReducers({
-
+    ticketReducer
 });
