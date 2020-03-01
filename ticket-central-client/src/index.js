d5c0c38e8e937839cfbfc478efc0c5e37daadecc
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-02-29 20:57:40 -0500

    Wrapping app with Auth0 context

--- ticket-central-client/src/index.js
+++ ticket-central-client/src/index.js
@@ -5,13 +5,34 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import configureStore from "./store/configureStore";
 import { Provider } from "react-redux";
+import {Auth0Provider} from "./Auth/Auth";
+import history from "./utils/history";
 
 const store = configureStore();
 
+// A function that routes the user to the right place
+// after login
+const onRedirectCallback = appState => {
+    history.push(
+        appState && appState.targetUrl
+            ? appState.targetUrl
+            : window.location.pathname
+    );
+};
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+
+        <Auth0Provider
+            domain={process.env.REACT_APP_AUTH0_DOMAIN}
+            client_id={process.env.REACT_APP_AUTH0_CLIENTID}
+            redirect_uri={process.env.REACT_APP_AUTH0_CALLBACK}
+            onRedirectCallback={onRedirectCallback}>
+
+            <App />
+
+        </Auth0Provider>
+
     </Provider>
     , document.getElementById('root'));
 
