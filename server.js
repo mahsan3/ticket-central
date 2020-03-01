dd12c6be3ebc023e44a810cf361d8f7abb8057bc
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 10:16:59 -0500

    Added middleware provided by Auth0 for validating JWT

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
+const jwt = require("express-jwt"); // Validate JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // Retrieve RSA keys from a JSON Web Key set (JWKS) endpoint
 const bodyParser = require('body-parser');
 const appRoutes = require('./api/routes/routes');
 const models = require('./api/models');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Create middleware for checking the JWT
+const checkJwt = jwt({
+    // Dynamically provide a signing key based on the kid in the header and the singing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-ttb978df.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer.
+    audience: `http://localhost:3001`,
+    issuer: `https://dev-ttb978df.auth0.com/`,
+    algorithms: ['RS256']
+});
 
 app.use('/', appRoutes);
 
