6dfc7b2a8ae8bfe3afa355a75b3333d4a2222c47
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 20:44:30 -0500

    Added toast container and styling

--- ticket-central-client/src/App.js
+++ ticket-central-client/src/App.js
@@ -4,11 +4,13 @@ import NavContainer from "./Navigation/containers/NavContainer";
 import {BrowserRouter, Route} from "react-router-dom";
 import HomeContainer from "./Home/containers/HomeContainer";
 import EditTicket from "./EditTicket/containers/EditTicket";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <BrowserRouter>
-
+        <ToastContainer />
         <NavContainer />
 
         <Route exact path="/" component={HomeContainer} />
