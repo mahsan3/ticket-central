f4267de1c4ccf7be6aa208864dcf7dc55ae9edf6
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 19:28:52 -0500

    Created ticket form component that binds to existing ticket data

---
+++ ticket-central-client/src/EditTicket/components/TicketForm.js
@@ -0,0 +1,89 @@
+import React, {useEffect, useState} from 'react';
+import { Multiselect } from 'multiselect-react-dropdown';
+
+function TicketForm(props) {
+
+    const [title, setTitle] = useState(props.ticketData.title);
+    const [description, setDescription] = useState(props.ticketData.description);
+    const [status, setStatus] = useState(props.ticketData.status.name);
+    const [assignedTo, setAssignedTo] = useState(props.ticketData.ticket_assigned_to.name);
+    const [tags, setTags] = useState(props.ticketData.tags);
+
+    useEffect(() => {
+
+    }, []);
+
+    const onSelect = (selectedList, selectedItem) => {
+        console.log(selectedList, selectedItem);
+        setTags(selectedList);
+    };
+
+    const onRemove = (selectedList, removedItem) => {
+        console.log(selectedList, removedItem);
+        setTags(selectedList);
+    };
+
+    return (
+        <form className="" onSubmit={props.handleSubmit} autoComplete="off">
+
+            <div className="form-group">
+                <label htmlFor="title">Title</label>
+                <input type="text" className="form-control" id="title"
+                        value={title}
+                       onChange={e => setTitle(e.target.value)} />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="desc">Description</label>
+                <textarea className="form-control" id="desc" rows="3"
+                          value={description}
+                          onChange={e => setDescription(e.target.value)}
+                ></textarea>
+            </div>
+
+            <div className="form-group">
+                <label>Status</label>
+                <select className="form-control">
+                    <option>Default select</option>
+                    {props.status.map(s => {
+                        return <option
+                                    value={s.id}
+                                    selected={s.name === status}
+                                    key={s.id}>
+                                    {s.name}
+                                </option>;
+                    })}
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label>Assigned To</label>
+                <select className="form-control">
+                    <option>Default select</option>
+                    {props.users.map(u => {
+                        return <option
+                            value={u.id}
+                            selected={u.name === assignedTo}
+                            key={u.id}>
+                            {u.name} ({u.email})
+                        </option>;
+                    })}
+                </select>
+            </div>
+
+            <div className="form-group">
+                <label>Tags</label>
+                <Multiselect
+                    options={props.tags} // Options to display in the dropdown
+                    selectedValues={tags} // Preselected value to persist in dropdown
+                    onSelect={onSelect} // Function will trigger on select event
+                    onRemove={onRemove} // Function will trigger on remove event
+                    displayValue="name" // Property name to display in the dropdown options
+                />
+            </div>
+
+        </form>
+    );
+}
+
+export default TicketForm;
