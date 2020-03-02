c01e3a03e80957cc00845496183bda75bad96e06
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 22:56:38 -0500

    Added component for creating tickets

---
+++ ticket-central-client/src/NewTicket/containers/NewTicket.js
@@ -0,0 +1,71 @@
+import React, {useEffect} from 'react';
+import {connect} from "react-redux";
+import {createTicket, loadTicketOptionData} from "../../store/actions/ticket.actions";
+import TicketForm from "../../common/TicketForm";
+
+function NewTicket(props) {
+
+    let initTicketValues = {
+        title: '',
+        description: '',
+        status: {
+            id: ''
+        },
+        ticket_assigned_to: {
+            id: ''
+        },
+        tags: []
+    };
+
+    useEffect(() => {
+        props.loadOptionalData();
+
+        return () => {
+        };
+
+    }, []);
+
+    const createTicket = ticket => {
+        console.log('Create new ticket', ticket);
+        props.newTicket(ticket);
+        props.history.push('/');
+    };
+
+    return (
+        <div className="row">
+            <div className="col">
+
+                <TicketForm
+                    handleSubmit={createTicket}
+                    ticketData={initTicketValues}
+                    tags={props.tags}
+                    users={props.users}
+                    status={props.status}
+                />
+
+            </div>
+        </div>
+    );
+}
+
+// Wire up this component to the store
+function mapStateToProps(state) {
+    return {
+        tags: state.ticketReducer.tags,
+        users: state.ticketReducer.users,
+        status: state.ticketReducer.status
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+
+    return {
+        loadOptionalData: () => dispatch(loadTicketOptionData()),
+        newTicket: ticket => dispatch(createTicket(ticket))
+    };
+
+}
+
+NewTicket = connect(mapStateToProps, mapDispatchToProps)(NewTicket);
+
+export default NewTicket;
