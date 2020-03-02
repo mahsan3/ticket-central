77da8b1ec3cc9aff26441bbd395454ef6687480e
Muhammad Ahsan <mm.ahsan@hotmail.com>
2020-03-01 21:40:11 -0500

    Registered delete epic

--- ticket-central-client/src/store/reducers/index.js
+++ ticket-central-client/src/store/reducers/index.js
@@ -1,12 +1,19 @@
 import {combineReducers} from "redux";
 import { combineEpics } from 'redux-observable';
-import {loadTicketsEpic, editTicketEpic, loadTicketOptionsEpic, updateTicketEpic} from '../epics/ticket.epic';
+import {
+    loadTicketsEpic,
+    editTicketEpic,
+    loadTicketOptionsEpic,
+    updateTicketEpic,
+    deleteTicketEpic
+} from '../epics/ticket.epic';
 import ticketReducer from "./ticket.reducer";
 
 export const rootEpic = combineEpics(
     loadTicketsEpic,
     loadTicketOptionsEpic,
-    updateTicketEpic
+    updateTicketEpic,
+    deleteTicketEpic
     // editTicketEpic
 );
 
